bc92b4516aeba7cc7f66485a02a2cd879c41519a
Juan Valenzuela <jvalenzuelaortega@gmail.com>
2022-06-06 20:56:38 -0400

    first commit, project base structure

---
+++ movielibraryweb/src/main/java/cl/mycompany/movielibraryweb/MovielibrarywebApplication.java
@@ -0,0 +1,13 @@
+package cl.mycompany.movielibraryweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovielibrarywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovielibrarywebApplication.class, args);
+	}
+
+}
---
+++ movielibraryweb/src/test/java/cl/mycompany/movielibraryweb/MovielibrarywebApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.mycompany.movielibraryweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielibrarywebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
