a57327ae0d7bd6f72c3eb8e61e0ed82a613b727b
Juan Valenzuela <jvalenzuelaortega@gmail.com>
2022-06-09 18:34:01 -0400

    ADD: bootstrap is added Bootstrap, Jquery is added to the project, also new html files and associated controller are configured

--- movielibraryweb/src/main/java/cl/mycompany/movielibraryweb/controller/HomeController.java
+++ movielibraryweb/src/main/java/cl/mycompany/movielibraryweb/controller/HomeController.java
@@ -7,18 +7,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public ModelAndView getHomePage(){
         return new ModelAndView("home");
-    }
-
-    @GetMapping("/new-register")
-    public ModelAndView getNewFormMovie(){
-        return new ModelAndView("new-register");
-    }
-
-    @GetMapping("/list-movies")
-    public ModelAndView getListMovies(){
-        return new ModelAndView("list-movies");
     }
 }
---
+++ movielibraryweb/src/main/java/cl/mycompany/movielibraryweb/controller/MovieController.java
@@ -0,0 +1,24 @@
+package cl.mycompany.movielibraryweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MovieController {
+
+    @GetMapping("/new-register")
+    public ModelAndView getNewMoviePage(){
+        return new ModelAndView("new-register");
+    }
+
+    @GetMapping("/list-movies")
+    public ModelAndView getListMoviesPage(){
+        return new ModelAndView("list-movies");
+    }
+
+    @GetMapping("/edit-movie")
+    public ModelAndView getEditMoviePage(){
+        return new ModelAndView("edit-movie");
+    }
+}
